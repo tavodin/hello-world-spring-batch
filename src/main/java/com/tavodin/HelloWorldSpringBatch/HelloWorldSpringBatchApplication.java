0cd8e76d4ba2b611b55673d2694198c3c5bbe7a4
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-11-02 16:00:32 -0300

    Project created, Job and Step configuration

---
+++ src/main/java/com/tavodin/HelloWorldSpringBatch/HelloWorldSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.tavodin.HelloWorldSpringBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tavodin/HelloWorldSpringBatch/HelloWorldSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tavodin.HelloWorldSpringBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
